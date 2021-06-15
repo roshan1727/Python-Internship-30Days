089e8d7b8514b33ae2296f1b9037d9d37b7b79bf
roshan <57836306+roshan1727@users.noreply.github.com>
2021-06-15 21:38:35 +0530

    commit

---
+++ Day1Challenge.py
@@ -0,0 +1,7 @@
+#day 1 task
+x=13
+y=10
+print('sum of x and y')
+print(x+y)
+print('maximum among x and y')
+print(max(x,y))
---
+++ Day4Challenge.py
@@ -0,0 +1,31 @@
+# 1)Create three variables (a,b,c) to same value of any integer & 
+# do the following
+# a) Divide a by 10
+# b) Multiply b by 50
+# c) Add c value by 60
+>>> a=b=c=2345
+>>> print(a//10)
+234
+>>> print(b*50)
+117250
+>>> print(c+60)
+2405
+# 2)Create a String variable of 5 characters and replace the 3rd
+# character with G
+>>> string="Rocky"
+>>> print(string.replace(string[2],"G"))
+RoGky
+# 3)Create two values (a,b) of int,float data type & convert the 
+# vise versa, Hint : convert a from int to float datatype & b from 
+# float to int datatype
+>>> a=9000
+>>> b=1155.5
+>>> print(type(a))
+<class 'int'>
+>>> print(float(a))
+9000.0
+>>> print(type(b))
+<class 'float'>
+>>> print(int(b))
+1155
+>>> 
