324f3a9b6b677cd69ec05050020988b43e3ef86e
roshan <57836306+roshan1727@users.noreply.github.com>
2021-06-15 21:32:06 +0530

    Day4

--- Day2Challenge.py
+++ Day2Challenge.py
@@ -1,74 +1,44 @@
-# Example Simple format
-# from tkinter import *
-# from tkinter import ttk
-# window=Tk()
-# window.title("Registration Screen")
-# window.geometry("300x300")
-# window.configure(background="Hot pink")
-# Firstname=Label(window,text="First Name").grid(row=0,column=0)
-# Lastname=Label(window,text="Last Name").grid(row=1,column=0)
-# Email=Label(window,text="Email Id").grid(row=2,column=0)
-# Mobile=Label(window,text="Contact Number").grid(row=3,column=0)
-# Firstname1=Entry(window).grid(row=0,column=1)
-# Lastname1=Entry(window).grid(row=1,column=1)
-# Email=Entry(window).grid(row=2,column=1)
-# Mobile1=Entry(window).grid(row=3,column=1)
-
-# def clicked():
-#     res="Welcome to "+ txt.get()
-#     lbl.configure(text=res)
-# btn=ttk.Button(window,text="Submit").grid(row=4,column=0)
-# window.mainloop()
-    
-
-# Employee registeration form
-from tkinter import *
-from tkinter import ttk
-
-window = Tk()
-#Declaring Window Title
-window.title("Employee Registration")
-#Declaring Window size
-window.geometry('650x550')
-#Declaring Window Color
-window.configure(background = "skyblue")
-label_0= Label(window,text="Employee Registration Form",font = ("bold",20))
-label_0.grid(row =0,column = 4)
-#below four fields are declared
-Firstname = Label(window ,text = "First Name").grid(row = 2,column = 0)
-LastName = Label(window ,text = "Last Name").grid(row = 3,column = 0)
-Email = Label(window ,text = "Email Id").grid(row = 4,column = 0)
-Mobile = Label(window ,text = "Contact Number").grid(row = 5,column = 0)
-a1 = Label(window ,text = "Contact Number").grid(row = 6,column = 0)
-pan = Label(window ,text = "Pan Number").grid(row = 7,column = 0)
-adhaar = Label(window ,text = "adhaar Number").grid(row = 8,column = 0)
-tele = Label(window ,text = "telephone Number").grid(row = 9,column = 0)
-college = Label(window ,text = "college name").grid(row = 10,column = 0)
-school = Label(window ,text = "school name").grid(row = 11,column = 0)
-insta = Label(window ,text = " instagram id").grid(row = 12,column = 0)
-roll = Label(window ,text = "roll number").grid(row = 13,column = 0)
-age = Label(window ,text = "age").grid(row = 14,column = 0)
-pnr = Label(window ,text = "pnr Number").grid(row = 15,column = 0)
-
-Firstname1 = Entry(window).grid(row = 2,column = 1)
-Lastname1 = Entry(window).grid(row = 3,column = 1)
-Email1 = Entry(window).grid(row = 4,column = 1)
-Mobile1 = Entry(window).grid(row = 5,column = 1)
-a11= Entry(window).grid(row = 6,column = 1)
-pan1 = Entry(window).grid(row = 7,column = 1)
-adhaar1 = Entry(window).grid(row = 8,column = 1)
-tele1 = Entry(window).grid(row = 9,column = 1)
-college1 = Entry(window).grid(row = 10,column = 1)
-school1 = Entry(window).grid(row = 11,column = 1)
-insta1 = Entry(window).grid(row = 12,column = 1)
-roll1 = Entry(window).grid(row = 13,column = 1)
-age1 = Entry(window).grid(row = 14,column = 1)
-pnr1 = Entry(window).grid(row = 15,column = 1)
-var1=IntVar()
-Checkbutton(window,text= "accept terms and condition", variable=var1).place(x=100,y=400)
-#fubction declaration
-def clicked():
-        res = "Welcome to " + txt.get()
-        lbl.configure(text= res)
-btn = ttk.Button(window ,text="Submit").place(x = 100,y=450)
-window.mainloop()
+>>> print("30 days 30 hour challenge")
+30 days 30 hour challenge
+>>> Hours="thirty"
+>>> print(Hours)
+thirty
+>>> Days="Thirty days"
+>>> print(Days[0])
+T
+>>> Challenge="I will win"
+>>> print(Challenge[7:10])
+win
+>>> print(len(Challenge))
+10
+>>> print(Challenge.lower())
+i will win
+>>> a="30 Days"
+>>> b="30 hours"
+>>> c=a+b
+>>> print(c)
+30 Days30 hours
+>>> c=a+" "+b
+>>> print(c)
+30 Days 30 hours
+>>> text="Thirty days and Thirty Hours"
+>>> x=text.casefold()
+>>> print(x)
+thirty days and thirty hours
+>>> x=text.capitalize()
+>>> print(x)
+Thirty days and thirty hours
+>>> x=text.find()
+Traceback (most recent call last):
+  File "<pyshell#20>", line 1, in <module>
+    x=text.find()
+TypeError: find() takes at least 1 argument (0 given)
+>>> x=text.find(a)
+>>> print(x)
+-1
+>>> x=text.isalpha()
+>>> print(x)
+False
+>>> x=text.isalnum()
+>>> print(x)
+False
